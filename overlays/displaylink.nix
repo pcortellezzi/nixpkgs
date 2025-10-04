c2803329f4fc13f41ea4aaa0638439bfc591bbf4
Philippe CORTELLEZZI <philippe.cortellezzi@gmail.com>
2025-10-04 18:46:19 -0300

    feat(displaylink): use evdi from overlay

--- overlays/displaylink.nix
+++ overlays/displaylink.nix
@@ -6,5 +6,6 @@ final: prev: {
       url = "https://www.synaptics.com/sites/default/files/exe_files/2025-09/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu6.2-EXE.zip";
       hash = "sha256-JQO7eEz4pdoPkhcn9tIuy5R4KyfsCniuw6eXw/rLaYE=";
     };
+    evdi = final.linuxPackages.evdi;
   });
 }
