19dd611d6bdf464b9e081b30dd64605783ed57ab
Philippe CORTELLEZZI <philippe.cortellezzi@gmail.com>
2025-10-04 16:00:21 -0300

    feat(displaylink): update to 6.2.0-30

--- overlays/displaylink.nix
+++ overlays/displaylink.nix
@@ -1,9 +1,10 @@
 final: prev: {
   # Surcharge la fonction displaylink pour utiliser notre version locale.
   displaylink = prev.displaylink.overrideAttrs (oldAttrs: {
+    version = "6.2.0-30";
     src = prev.fetchurl {
-      url = "https://www.synaptics.com/sites/default/files/exe_files/2024-10/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu6.1-EXE.zip";
-      hash = "sha256-RJgVrX+Y8Nvz106Xh+W9N9uRLC2VO00fBJeS8vs7fKw=";
+      url = "https://www.synaptics.com/sites/default/files/exe_files/2025-09/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu6.2-EXE.zip";
+      hash = "sha256-JQO7eEz4pdoPkhcn9tIuy5R4KyfsCniuw6eXw/rLaYE=";
     };
   });
 }
