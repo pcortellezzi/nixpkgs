7da263b1ea12232b9a5c0214c4e26aa9d1b57357
Philippe CORTELLEZZI <philippe.cortellezzi@gmail.com>
2025-10-04 18:14:53 -0300

    feat(evdi): update to 1.14.11

---
+++ overlays/evdi.nix
@@ -0,0 +1,15 @@
+final: prev: {
+  linuxPackages = prev.lib.recursiveUpdate prev.linuxPackages {
+    evdi = prev.linuxPackages.evdi.overrideAttrs (oldAttrs: {
+      version = "1.14.11";
+      src = oldAttrs.src.override {
+        tag = "v1.14.11";
+        hash = "sha256-SxYUhu76vwgCQgjOYVpvdWsFpNcyzuSjZe3x/v566VU=";
+      };
+      nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ prev.pkg-config ];
+      postPatch = ''
+        sed -i '/\/etc\/os-release/d' module/Makefile
+      '';
+    });
+  };
+}
