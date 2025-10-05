cf828194068a1ff347cfc9265f1a01e43c0ecfab
Philippe CORTELLEZZI <philippe.cortellezzi@gmail.com>
2025-10-05 06:13:45 -0300

    feat: Add workflow to auto-update evdi package

--- overlays/evdi.nix
+++ overlays/evdi.nix
@@ -2,8 +2,10 @@ final: prev: {
   linuxPackages_latest = prev.lib.recursiveUpdate prev.linuxPackages_latest {
     evdi = prev.linuxPackages_latest.evdi.overrideAttrs (oldAttrs: {
       version = "1.14.11";
-      src = oldAttrs.src.override {
-        tag = "v1.14.11";
+      src = final.fetchFromGitHub {
+        owner = "DisplayLink";
+        repo = "evdi";
+        rev = "v${oldAttrs.version}";
         hash = "sha256-SxYUhu76vwgCQgjOYVpvdWsFpNcyzuSjZe3x/v566VU=";
       };
       nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ prev.pkg-config ];
