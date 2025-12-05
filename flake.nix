d0b28081c5c528fcf08d3176e5f7347fb00814cc
Philippe CORTELLEZZI <philippe.cortellezzi@gmail.com>
2025-12-05 06:53:23 -0300

    chore: update nixpkgs to 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Paquets personnels de Philippe";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
