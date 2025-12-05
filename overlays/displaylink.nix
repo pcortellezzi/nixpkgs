f22448d5678e8a9356dc6f9486ca5420cf3b1db7
Cortellezzi Philippe (LGM GUYANE) <philippe.cortellezzi.external@cnes.fr>
2025-12-05 08:51:01 -0300

    feat: remove evdi and use displaylink from offical repo

--- overlays/displaylink.nix
+++ overlays/displaylink.nix
@@ -1,8 +1,8 @@
 final: prev: {
   displaylink = prev.displaylink.overrideAttrs (oldAttrs: {
     src = prev.fetchurl {
-      url = "https://www.synaptics.com/sites/default/files/Ubuntu/pool/stable/non-free/amd64/displaylink-driver-6.2.0-30_amd64.deb";
-      hash = "sha256-AWnFc6h4bD9PA3PvE3oPRMoWuBb6kEFE8q7SEgBvPMU=";
+      url = "https://www.synaptics.com/sites/default/files/exe_files/2025-09/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu6.2-EXE.zip";
+      hash = "sha256-JQO7eEz4pdoPkhcn9tIuy5R4KyfsCniuw6eXw/rLaYE=";
     };
   });
 }
