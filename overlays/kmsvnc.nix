f1aba3069267d64180ebe2095156c35f349401cb
Philippe CORTELLEZZI <philippe.cortellezzi@gmail.com>
2026-06-25 19:57:41 -0300

    kmsvnc: update to 59727d6

--- overlays/kmsvnc.nix
+++ overlays/kmsvnc.nix
@@ -3,8 +3,8 @@ final: prev: {
     src = prev.fetchFromGitHub {
       owner = "pcortellezzi";
       repo = "kmsvnc";
-      rev = "add1c34c6fc2110d99503db12ce395095e9b60f7";
-      hash = "sha256-XjyTCPbkHgxuQrnLjuUwjKmQg8Pq4Y4rdsvYPWsejWs=";
+      rev = "59727d6384367574dcd32d1f398685691e060a66";
+      hash = "sha256-ujwTcUYghdAf8izg6wc7K0alyjX3shmAnk1pfg/n7H4=";
     };
     version = "${old.version}-embed-cursor";
   });
