97c430af622e27191ac50239490a14e29433a33d
Philippe CORTELLEZZI <philippe.cortellezzi@gmail.com>
2026-06-25 18:54:08 -0300

    kmsvnc: update to latest embed-cursor commit 5199501

--- overlays/kmsvnc.nix
+++ overlays/kmsvnc.nix
@@ -3,8 +3,8 @@ final: prev: {
     src = prev.fetchFromGitHub {
       owner = "pcortellezzi";
       repo = "kmsvnc";
-      rev = "feature/embed-cursor";
-      hash = "sha256-lm5zf0tj0qPooYv13EaPpR2KEQSSSpp9L00hwya1wrI=";
+      rev = "51995012548b68f46a410468ffe81dce4116426c";
+      hash = "sha256-T7H0k9hEI92b7ibkYC2ByXJkeXh83Fb/crDPQIjvf5c=";
     };
     version = "${old.version}-embed-cursor";
   });
